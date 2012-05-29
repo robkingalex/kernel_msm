cfdb2d2b2a7e12d2005406b65ceee341d3ad5e77
Johannes Weiner <hannes@cmpxchg.org>
2012-05-29 15:06:24 -0700

    kernel: cgroup: push rcu read locking from css_is_ancestor() to callsite
    Library functions should not grab locks when the callsites can do it,
    even if the lock nests like the rcu read-side lock does.
    
    Push the rcu_read_lock() from css_is_ancestor() to its single user,
    mem_cgroup_same_or_subtree() in preparation for another user that may
    already hold the rcu read-side lock.
    
    Change-Id: I0ec7ec0059ed588d8f85bc9be8fdc42ce0ca7f5d
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Acked-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Acked-by: Li Zefan <lizf@cn.fujitsu.com>
    Cc: Li Zefan <lizf@cn.fujitsu.com>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Git-commit: 91c63734f6908425903aed69c04035592f18d398
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1117,12 +1117,16 @@ void mem_cgroup_update_lru_size(struct lruvec *lruvec, enum lru_list lru,
 static bool mem_cgroup_same_or_subtree(const struct mem_cgroup *root_memcg,
 		struct mem_cgroup *memcg)
 {
-	if (root_memcg != memcg) {
-		return (root_memcg->use_hierarchy &&
-			css_is_ancestor(&memcg->css, &root_memcg->css));
-	}
+	bool ret;
 
-	return true;
+	if (root_memcg == memcg)
+		return true;
+	if (!root_memcg->use_hierarchy)
+		return false;
+	rcu_read_lock();
+	ret = css_is_ancestor(&memcg->css, &root_memcg->css);
+	rcu_read_unlock();
+	return ret;
 }
 
 int task_in_mem_cgroup(struct task_struct *task, const struct mem_cgroup *memcg)

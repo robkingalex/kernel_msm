bdc97600e870deb3781db9ab5b97dd32ce5e3efe
Patrick Daly <pdaly@codeaurora.org>
2013-08-08 11:59:23 -0700

    msm: cpufreq: Update frequency index
    A deprecated api can be used to limit the minimum and maximum cpu speeds.
    Another driver temporarily needs to switch back to this api, instead of
    using the new version. Make this api functional again by ensuring a stale
    value is updated properly.
    
    Change-Id: Ia63772e43ec534cf2bb58e7627a87315dae5d891
    Signed-off-by: Patrick Daly <pdaly@codeaurora.org>

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -125,6 +125,7 @@ static int set_cpu_freq(struct cpufreq_policy *policy, unsigned int new_freq,
 	struct cpufreq_freqs freqs;
 	struct cpu_freq *limit = &per_cpu(cpu_freq_info, policy->cpu);
 	struct sched_param param = { .sched_priority = MAX_RT_PRIO-1 };
+	struct cpufreq_frequency_table *table;
 
 	if (limit->limits_init) {
 		if (new_freq > limit->allowed_max) {
@@ -137,6 +138,12 @@ static int set_cpu_freq(struct cpufreq_policy *policy, unsigned int new_freq,
 			pr_debug("min: limiting freq to %d\n", new_freq);
 		}
 	}
+
+	/* limits applied above must be in cpufreq table */
+	table = cpufreq_frequency_get_table(policy->cpu);
+	if (cpufreq_frequency_table_target(policy, table, new_freq,
+		CPUFREQ_RELATION_H, &index))
+		return -EINVAL;
 
 	freqs.old = policy->cur;
 	freqs.new = new_freq;
@@ -169,7 +176,6 @@ static int set_cpu_freq(struct cpufreq_policy *policy, unsigned int new_freq,
 
 	if (!ret)
 		cpufreq_notify_transition(&freqs, CPUFREQ_POSTCHANGE);
-	}
 
 	/* Restore priority after clock ramp-up */
 	if (freqs.new > freqs.old && saved_sched_policy >= 0) {
@@ -215,6 +221,9 @@ static int msm_cpufreq_target(struct cpufreq_policy *policy,
 		ret = -EINVAL;
 		goto done;
 	}
+
+	if (table[index].frequency == policy->cur)
+		goto done;
 
 	pr_debug("CPU[%d] target %d relation %d (%d-%d) selected %d\n",
 		policy->cpu, target_freq, relation,

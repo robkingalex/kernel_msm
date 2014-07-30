e2fcbed5d9c2c711b6491b789ae6733675ef053d
Paul Reioux <reioux@gmail.com>
2014-07-30 18:22:01 -0500

    Sound Control: Misc clean up for newer WCD9xxx SOCs
    Bump driver version to 3.4
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- sound/soc/codecs/wcd9310.c
+++ sound/soc/codecs/wcd9310.c
@@ -8401,6 +8401,8 @@ struct snd_kcontrol_new *gpl_faux_snd_controls_ptr =
 		(struct snd_kcontrol_new *)tabla_snd_controls;
 struct snd_soc_codec *fauxsound_codec_ptr;
 EXPORT_SYMBOL(fauxsound_codec_ptr);
+int wcd9xxx_hw_revision;
+EXPORT_SYMBOL(wcd9xxx_hw_revision);
 #endif
 
 static int tabla_codec_probe(struct snd_soc_codec *codec)

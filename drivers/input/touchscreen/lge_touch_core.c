141f3c490bf8c0c61d04d1676005e968525892af
savoca <adeddo27@gmail.com>
2014-08-03 13:58:21 +0000

    lge_touch_core: Removed unused int variable retry

--- drivers/input/touchscreen/lge_touch_core.c
+++ drivers/input/touchscreen/lge_touch_core.c
@@ -614,7 +614,6 @@ void* get_touch_handle(struct i2c_client *client)
 int touch_i2c_read(struct i2c_client *client, u8 reg, int len, u8 *buf)
 {
 #define LGETOUCH_I2C_RETRY 10
-	int retry = 0;
 	struct i2c_msg msgs[] = {
 		{
 			.addr = client->addr,

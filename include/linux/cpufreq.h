c9c1ccc1bd2838a0a8116e0cc8d1fa0bbe7fa134
ion-storm <defconoii@gmail.com>
2014-08-14 00:37:11 -0400

    cpufreq: bluactive: initial version
    Based of Google, CAF, CM, myfluxi and franciscofranco Interactive

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -416,6 +416,9 @@ extern struct cpufreq_governor cpufreq_gov_nightmare;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLUACTIVE)
+extern struct cpufreq_governor cpufreq_gov_bluactive;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bluactive)
 #endif
 
 

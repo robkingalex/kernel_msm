73650fa0bda20de8d4f63357a4ca9998ece56c21
ion-storm <defconoii@gmail.com>
2014-08-17 01:14:38 -0400

    Linux 3.4.103

--- net/core/iovec.c
+++ net/core/iovec.c
@@ -51,6 +51,7 @@ int verify_iovec(struct msghdr *m, struct iovec *iov, struct sockaddr_storage *a
 		m->msg_name = address;
 	} else {
 		m->msg_name = NULL;
+		m->msg_namelen = 0;
 	}
 
 	size = m->msg_iovlen * sizeof(struct iovec);

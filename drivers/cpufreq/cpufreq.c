8a86cab365a0419f7ee26ae17dfcc664a7e4297c
ion-storm <defconoii@gmail.com>
2014-08-27 08:26:42 -0400

    Revert: msm: krait: added userspace voltage control
    Conflicts:
    	drivers/cpufreq/cpufreq.c
    
    Conflicts:
    	drivers/cpufreq/cpufreq.c
    
    Conflicts:
    	drivers/cpufreq/cpufreq.c (reverted from commit bf2fad3456e1a3961083e5228a606749686ced74) (reverted from commit 95e52c1d0a1af9c62b425f94e83646e2dee4c79e)

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -694,6 +694,14 @@ static ssize_t show_bios_limit(struct cpufreq_policy *policy, char *buf)
 	return sprintf(buf, "%u\n", policy->cpuinfo.max_freq);
 }
 
+#ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
+extern ssize_t show_UV_mV_table(struct cpufreq_policy *policy,
+					char *buf);
+
+extern ssize_t store_UV_mV_table(struct cpufreq_policy *policy,
+					const char *buf, size_t count);
+#endif
+
 #ifdef CONFIG_GPU_VOLTAGE_TABLE
 ssize_t show_gpu_mv_table(struct cpufreq_policy *policy, char *buf)
 {
@@ -725,7 +733,9 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
-
+#ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
+cpufreq_freq_attr_rw(UV_mV_table);
+#endif
 #ifdef CONFIG_GPU_VOLTAGE_TABLE
 cpufreq_freq_attr_rw(gpu_mv_table);
 #endif
@@ -743,7 +753,9 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
-
+#ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
+	&UV_mV_table.attr,
+#endif
 #ifdef CONFIG_GPU_VOLTAGE_TABLE
 	&gpu_mv_table.attr,
 #endif

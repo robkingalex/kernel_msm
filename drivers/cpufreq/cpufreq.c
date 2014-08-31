41aaf6ee5604956ff03b93fa15efdf8aecdaeb25
ion-storm <defconoii@gmail.com>
2014-08-31 03:34:03 -0400

    fixup cpufreq & gpu control

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -34,10 +34,8 @@
 
 #include <trace/events/power.h>
 
-#ifdef CONFIG_GPU_VOLTAGE_TABLE
 extern ssize_t get_gpu_vdd_levels_str(char *buf);
 extern void set_gpu_vdd_levels(int uv_tbl[]);
-#endif
 
 /**
  * The "cpufreq driver" - the arch- or hardware-dependent low
@@ -710,6 +708,20 @@ static ssize_t store_UV_mV_table(struct cpufreq_policy *policy, const char *buf,
 
 #endif
 
+ssize_t show_gpu_mv_table(struct cpufreq_policy *policy, char *buf)
+{
+        return get_gpu_vdd_levels_str(buf);
+}
+
+ssize_t store_gpu_mv_table(struct cpufreq_policy *policy, const char *buf, size_t count)
+{
+        unsigned int ret = -EINVAL;
+        unsigned int u[3];
+        ret = sscanf(buf, "%d %d %d", &u[0], &u[1], &u[2]);
+        set_gpu_vdd_levels(u);
+        return count;
+}
+
 cpufreq_freq_attr_ro_perm(cpuinfo_cur_freq, 0400);
 cpufreq_freq_attr_ro(cpuinfo_min_freq);
 cpufreq_freq_attr_ro(cpuinfo_max_freq);
@@ -728,12 +740,7 @@ cpufreq_freq_attr_rw(scaling_setspeed);
 #ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
 cpufreq_freq_attr_rw(UV_mV_table);
 #endif
-#ifdef CONFIG_GPU_VOLTAGE_TABLE
 cpufreq_freq_attr_rw(gpu_mv_table);
-#endif
-#ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
-cpufreq_freq_attr_rw(UV_mV_table);
-#endif
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -751,12 +758,7 @@ static struct attribute *default_attrs[] = {
 #ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
 	&UV_mV_table.attr,
 #endif
-#ifdef CONFIG_GPU_VOLTAGE_TABLE
 	&gpu_mv_table.attr,
-#endif
-#ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
-	&UV_mV_table.attr,
-#endif
 	NULL
 };
 

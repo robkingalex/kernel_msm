6e140ae33ccc6fe3d593bac9a082140d223a65a0
ion-storm <defconoii@gmail.com>
2014-10-01 12:41:58 -0400

    Revert "remove unneccesary wakelock while off charger"
    This reverts commit f78719d7cf7f3e8377dbd9fb27497f70f24152cd.

--- drivers/power/pm8921-charger.c
+++ drivers/power/pm8921-charger.c
@@ -4685,6 +4685,8 @@ static int __devinit pm8921_charger_probe(struct platform_device *pdev)
 	}
 
 	enable_irq_wake(chip->pmic_chg_irq[USBIN_VALID_IRQ]);
+	enable_irq_wake(chip->pmic_chg_irq[USBIN_OV_IRQ]);
+	enable_irq_wake(chip->pmic_chg_irq[USBIN_UV_IRQ]);
 	if (!chip->ext_batt_temp_monitor)
 		enable_irq_wake(chip->pmic_chg_irq[BAT_TEMP_OK_IRQ]);
 	enable_irq_wake(chip->pmic_chg_irq[VBATDET_LOW_IRQ]);

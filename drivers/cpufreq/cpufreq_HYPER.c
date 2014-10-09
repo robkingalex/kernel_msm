ea55d0e961886b42d598a469a1d5dd4556d9bc4d
ion-storm <defconoii@gmail.com>
2014-10-09 17:24:43 -0400

    adjust UP_THRESHOLD_AT_MIN_FREQ to 75 original: 40

--- drivers/cpufreq/cpufreq_HYPER.c
+++ drivers/cpufreq/cpufreq_HYPER.c
@@ -48,7 +48,7 @@
 #define FREQ_STEP				(55)
 #define DEFAULT_FREQ_BOOST_TIME			(500000)
 #define MAX_FREQ_BOOST_TIME			(5000000)
-#define UP_THRESHOLD_AT_MIN_FREQ		(85)
+#define UP_THRESHOLD_AT_MIN_FREQ		(75)
 #define FREQ_FOR_RESPONSIVENESS			(702000)
 
 static u64 hyper_freq_boosted_time;

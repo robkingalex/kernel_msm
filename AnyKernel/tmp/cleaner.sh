54dcbb8216114f3b1df5c2eb26eb8eeab22c8a33
ion-storm <defconoii@gmail.com>
2014-11-17 19:08:43 -0500

    fix dmesg touch_boost spam, this kernel has an in-kernel solution

--- AnyKernel/tmp/cleaner.sh
+++ AnyKernel/tmp/cleaner.sh
@@ -1,9 +1,12 @@
 #!/sbin/sh
 
-# delete mpdecision & thermald because it is no longer needed
-rm -rf /system/bin/thermald
-rm -rf /system/bin/mpdecision
-rm -rf /system/lib/hw/power.msm8960.so
+# move mpdecision & thermald because it is no longer needed
+mv /system/lib/hw/power.msm8960.so /system/lib/hw/power.msm8960.so-bak;
+mv /system/lib/hw/power.mako.so /system/lib/hw/power.mako.so-bak;
+mv /system/bin/thermal-engine-hh /system/bin/thermal-engine-hh-bak;
+mv /system/bin/mpdecision /system/bin/mpdecision-bak;
+mv /system/bin/thermald /system/bin/thermald-bak;
+
 # clean init.d scripts
 INITD_DIR=/system/etc/init.d
 # Trinity

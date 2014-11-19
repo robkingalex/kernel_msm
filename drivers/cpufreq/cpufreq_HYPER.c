44773b9e1e3463eb9434e473e33df3db3a9d8415
ion-storm <defconoii@gmail.com>
2014-11-19 12:49:19 -0500

    cpufreq_HYPER: up_threshold_at_min_freq 70 was too high, 50 seems like a good battery/performance setting

--- drivers/cpufreq/cpufreq_HYPER.c
+++ drivers/cpufreq/cpufreq_HYPER.c
@@ -47,7 +47,7 @@
 #define FREQ_STEP				(55)
 #define DEFAULT_FREQ_BOOST_TIME			(500000)
 #define MAX_FREQ_BOOST_TIME			(5000000)
-#define UP_THRESHOLD_AT_MIN_FREQ		(70)
+#define UP_THRESHOLD_AT_MIN_FREQ		(50)
 #define FREQ_FOR_RESPONSIVENESS			(1512000)
 
 static u64 hyper_freq_boosted_time;
